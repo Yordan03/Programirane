4fbe714e9235bde1e60852adf20bb1c6f1cc1e2f
Yordan03 <47415736+Yordan03@users.noreply.github.com>
2019-02-08 09:39:25 +0300

    Add files via upload

---
+++ Group2Task1B.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication65
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 238;
+            int b = 345;
+            if (a < b)
+            {
+                Console.WriteLine(b + "is bigger");
+            }
+        }
+    }
+}
---
+++ Group2Task1C.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication67
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 238;
+            int b = 345;
+            int sum = Math.Max(a, b);
+            Console.WriteLine(sum);
+
+        }
+    }
+}
